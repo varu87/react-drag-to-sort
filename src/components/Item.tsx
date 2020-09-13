572ad1e47b180adecad2e64669f33dc009b7cd96
varu87 <varu87@gmail.com>
2020-09-13 19:58:35 +1000

    added animation

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -6,6 +6,12 @@ export interface IItem {
 	title: string;
 }
 
+export enum DragDirection {
+	'NONE',
+	'UP',
+	'DOWN'
+}
+
 interface IItemProps {
 	item: IItem;
 	index: number;
@@ -14,6 +20,11 @@ interface IItemProps {
 	onDragOver: (e: React.DragEvent<HTMLDivElement>) => void;
 	onDragLeave: (e: React.DragEvent<HTMLDivElement>) => void;
 	onDrop: (e: React.DragEvent<HTMLDivElement>) => void;
+}
+
+interface IStyleProps {
+	index: number;
+	draggedOver: number;
 }
 
 export const Item: React.FC<IItemProps> = ({
@@ -33,17 +44,27 @@ export const Item: React.FC<IItemProps> = ({
 			onDragOver={onDragOver}
 			onDragLeave={onDragLeave}
 			onDrop={onDrop}
+			index={index}
+			draggedOver={draggedOver}
 		>
 			<div>{item.title}</div>
 		</Container>
 	);
 };
 
-const Container = styled.div`
+const Container =
+	styled.div <
+	IStyleProps >
+	`
 	display: grid;
 	grid-template-columns: 1fr;
 	padding: 20px;
-	border: 1px solid #000;
 	border-radius: 5px;
+	position: relative;
 	margin-bottom: 5px;
+	border: ${({ index, draggedOver }) =>
+		draggedOver > -1 && index === draggedOver ? '2px dashed #000' : '1px solid #000'};
+	margin-left: ${({ index, draggedOver }) => (draggedOver > -1 && index === draggedOver ? '5%' : '0')};
+	color: ${({ index, draggedOver }) => (draggedOver > -1 && index === draggedOver ? 'transparent' : '#000')};
+	transition: margin-left 0.1s;
 `;
