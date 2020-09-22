45bf6241ac8b504a3252a4e2db2b8b2387ba04bb
varu87 <varu87@gmail.com>
2020-09-22 22:11:36 +1000

    Updates

---
+++ precache-manifest.2485e361392bf1c63bfb1e679582b57e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d4e4a4bc81e0246813fe4451ef816c",
+    "url": "/react-drag-to-sort/index.html"
+  },
+  {
+    "revision": "509ab230df041ffe93d8",
+    "url": "/react-drag-to-sort/static/css/main.e6303fc6.chunk.css"
+  },
+  {
+    "revision": "87a5de371c218cc4bc9f",
+    "url": "/react-drag-to-sort/static/js/2.9fa8b991.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-drag-to-sort/static/js/2.9fa8b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509ab230df041ffe93d8",
+    "url": "/react-drag-to-sort/static/js/main.5ca7d97b.chunk.js"
+  },
+  {
+    "revision": "1d3600998af9c0e8bfdd",
+    "url": "/react-drag-to-sort/static/js/runtime-main.1a4c062f.js"
+  }
+]);
